74841f799815fd1975cb92c40f8bc0be51a769de
Asep Septiadi <mcreator.id.dev@gmail.com>
2018-08-29 17:25:47 +0700

    Upload #1
    Paket Backend

---
+++ datasiswa-api/connection.php
@@ -0,0 +1,13 @@
+<?php
+	$server = "localhost";
+	$server_username = "id6813787_apiportal";
+	$server_password = "!!&21adi";
+	$database_name =  "id6813787_db_apiportal";
+
+	$conn = new mysqli(
+		$server, 
+		$server_username, 
+		$server_password, 
+		$database_name
+	);
+?>
